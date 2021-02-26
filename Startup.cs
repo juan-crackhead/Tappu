046105039d3c0ea750e1cfe21dc1ea0bab2bacce
Developer ✔️ <euanwalker08@outlook.com>
2021-02-26 18:30:40 +0000

    added bullshit

---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tappu
+{
+    public partial class StartupWindow : Form
+    {
+        public StartupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartupWindow_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
