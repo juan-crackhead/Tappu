d517fddc18527445a4290fa2e6837ee70db3d5d8
Developer ✔️ <euanwalker08@outlook.com>
2021-02-26 17:31:17 +0000

    Add project files.

---
+++ Tappu.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Game
+{
+    partial class TappuWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openAnimation = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // openAnimation
+            // 
+            this.openAnimation.Interval = 16;
+            // 
+            // TappuWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "TappuWindow";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tappu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer openAnimation;
+    }
+}
---
+++ Tappu.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class TappuWindow : Form
+    {
+        public TappuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Strings and other stuff
+            string settingslocation = @"C:\Tappu\tappudata\settings\";
+
+
+
+            //Initialize the Screen Mode
+            BackgroundImageLayout = ImageLayout.Zoom;
+
+            try
+            {
+                if (!File.Exists(settingslocation + "screenmode" + ".txt"))
+                {
+                    System.IO.File.Move(settingslocation + "screenmode.tappusetting", settingslocation + "screenmode.txt");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Settings are Broken, Re-install Tappu");
+            }
+
+            string text = File.ReadAllText(settingslocation + "screenmode" + ".txt");
+
+            if (text.Equals("Maximized"))
+            {
+                WindowState = FormWindowState.Maximized;
+                FormBorderStyle = FormBorderStyle.None;
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+                FormBorderStyle = FormBorderStyle.FixedSingle;
+            }
+
+
+
+            //Initialize Skin
+            try
+            {
+                if (!File.Exists(settingslocation + "selectedskin" + ".txt"))
+                {
+                    File.Move(settingslocation + "selectedskin.tappusetting", settingslocation + "selectedskin.txt");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Settings are Broken, Re-install Tappu");
+            }
+            //see what skin is selected in settings
+            string SelectedSkin = File.ReadAllText(settingslocation + "selectedskin" + ".txt");
+            //Set background photo
+            BackgroundImage = Image.FromFile(@"C:/Tappu/tappuskins/" + SelectedSkin + "/textures/mainmenu/menuback.jpg");
+
+
+            //End of settings loading
+
+            //Plays startup sound in skin folder
+            SoundPlayer simpleSound = new SoundPlayer("C:/Tappu/tappuskins/" + SelectedSkin + "/sounds/startupsound.wav");
+            simpleSound.Play();
+        }
+    }
+}
