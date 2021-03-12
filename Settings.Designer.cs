d0b05b6d4199e2582bd10c87d93cb75db4e9f945
Developer ✔️ <euanwalker08@outlook.com>
2021-03-12 22:10:56 +0000

    uh oh

--- Settings.Designer.cs
+++ Settings.Designer.cs
@@ -30,21 +30,41 @@ namespace Tappu
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(-11, -10);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(172, 684);
+            this.panel1.Size = new System.Drawing.Size(269, 677);
             this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Kamikaze", 33.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 97);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "settings!";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // SettingsWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(615, 675);
+            this.ClientSize = new System.Drawing.Size(1106, 665);
             this.Controls.Add(this.panel1);
             this.Cursor = System.Windows.Forms.Cursors.Cross;
             this.MaximizeBox = false;
@@ -54,6 +74,7 @@ namespace Tappu
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Settings";
             this.TopMost = true;
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -61,5 +82,6 @@ namespace Tappu
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
